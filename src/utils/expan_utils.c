c33bc17fc2116ae1483e01bdb14fb1e39226b481
Mepokam Okoye Yda Chioma <myda-chi@lab1r6s11.42abudhabi.ae>
2025-07-21 20:25:42 +0400

    the echo 858 is fixed

--- src/utils/expan_utils.c
+++ src/utils/expan_utils.c
@@ -6,7 +6,7 @@
 /*   By: myda-chi <myda-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 18:04:36 by myda-chi          #+#    #+#             */
-/*   Updated: 2025/07/21 20:06:38 by myda-chi         ###   ########.fr       */
+/*   Updated: 2025/07/21 20:22:17 by myda-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,20 @@ static int	is_variable_char(char c)
 	return (ft_isalnum(c) || c == '_' || c == '?' || c == '{');
 }
 
+static char	*handle_double_dollar(char *result, t_shell_state *state)
+{
+    char	*temp;
+    char	*pid_str;
+
+    pid_str = ft_itoa(state->shell_pid);
+    if (!pid_str)
+        return (result);
+    temp = ft_strjoin(result, pid_str);
+    free(result);
+    free(pid_str);
+    return (temp);
+}
+
 char	*expand_variables(char *str, t_shell_state *state)
 {
 	char	*result;
@@ -65,11 +79,14 @@ char	*expand_variables(char *str, t_shell_state *state)
 	if (!result)
 		return (NULL);
 	i = 0;
-	if (str[i] == '$' && str[i + 1] && str[i + 1] == '$')
-		return (ft_itoa(state->shell_pid)); // to update with a function that attaches extra strings if available
 	while (str[i])
 	{
-		if (str[i] == '$' && str[i + 1] && is_variable_char(str[i + 1]))
+		if (str[i] == '$' && str[i + 1] && str[i + 1] == '$')
+		{
+			result = handle_double_dollar(result, state);
+			i +=2;
+		}
+		else if (str[i] == '$' && str[i + 1] && is_variable_char(str[i + 1]))
 			result = handle_variable(result, str, &i, state); 
 		else
 			result = handle_regular_char(result, str[i++]);
